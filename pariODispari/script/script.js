ec63d81277da27c1e8a613c8462200fc72e3f49e
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-08 03:10:33 +0100

    L'esercizio è completo ma c'è un piccolo errore che non capisco da dove proviene, a volte la somma di numeri è pari ma i sistema la segna dispari, risoluzione del problema in corso

--- pariODispari/script/script.js
+++ pariODispari/script/script.js
@@ -18,3 +18,42 @@ if (numeroUtente > 5) {
 if (numeroUtente < 1) {
     numeroUtente = prompt("Inserisci un numero da 1 a 5, non minore di 1: ");
 }
+
+const numeroPC = inrangeRandomNumberGenerator(1,5);
+
+console.log(numeroPC);
+
+let sumOfNumbers = numeroUtente + numeroPC;
+
+
+let isPariSaver = isPari(sumOfNumbers);
+
+console.log(isPariSaver)
+
+if ( sceltaUtente === isPariSaver) {
+    console.log("Hey, hai vinto!");
+}
+
+else {
+    
+    console.log("Whoopsie, vince il pc!");
+}
+
+
+// Sezione funzioni per comodità
+
+function inrangeRandomNumberGenerator(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function isPari (number) {
+    if (number % 2 === 0) {
+        return "pari";
+    }
+
+    else if (number % 2 !== 0) {
+        return "dispari";
+    }
+}
